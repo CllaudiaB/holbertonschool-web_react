6eeb349f1b2b7ad2203b819e2298a4dbb3adf495
CllaudiaB <buraanna27@gmail.com>
2025-01-27 17:52:04 +0100

    task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -17,3 +17,32 @@ interface printTeacher {
 const printTeacher: printTeacher = function(firstName, lastName) {
     return `${firstName.charAt(0).toUpperCase()}. ${lastName}`;
 }
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
