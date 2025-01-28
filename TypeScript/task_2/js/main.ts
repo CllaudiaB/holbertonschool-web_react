9edf094785ad26add8b39c9fd28478523a431f79
CllaudiaB <buraanna27@gmail.com>
2025-01-28 09:44:51 +0100

    task 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -42,6 +42,18 @@ function createEmployee(salary: number | string): Teacher | Director {
     if (typeof salary === "number" && salary < 500)  {
             return new Teacher();
         } else {
-            return new Director()
+            return new Director();
         }
+}
+
+function isDirector(employee: Teacher | Director): employee is Director {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Teacher | Director) {
+    if (isDirector(employee)) {
+       return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
 }
