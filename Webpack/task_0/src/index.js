b898050cecd85278c7ccb37992aab24f9d137ab9
CllaudiaB <buraanna27@gmail.com>
2025-01-30 12:49:56 +0100

    task 0

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+const p1 = $("<p>Holberton Dashboard</p>");
+const p2 = $("<p>Dashboard data for the students</p>");
+const p3 = $("<p>Copyright - Holberton School</p>");
+
+$("body").append(p1).append(p2).append(p3);
