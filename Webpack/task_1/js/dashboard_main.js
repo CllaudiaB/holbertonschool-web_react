18b9b82e356e574f16f9ab5251fa08f906c70c38
CllaudiaB <buraanna27@gmail.com>
2025-01-30 15:12:21 +0100

    task 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,29 @@
+import $ from "jquery";
+import _ from "lodash";
+
+
+const p1 = $("<p>Holberton Dashboard</p>");
+const p2 = $("<p>Dashboard data for the students</p>");
+const button = $("<button>Click here to get started</button>");
+const p3 = $("<p id ='count'></p>");
+const p4 = $("<p>Copyright - Holberton School</p>");
+
+$("body").append(p1)
+    .append(p2)
+    .append(button)
+    .append(p3)
+    .append(p4);
+
+let count = 0;
+
+function updateCounter() {
+    count += 1;
+  $("#count").text(count + " clicks on the button");
+}
+
+const debouncedUpdateCounter = _.debounce(updateCounter, 500, {
+    leading: true,
+    trailing: false,
+});
+
+$("button").on("click", debouncedUpdateCounter);
