fbc367aa8f4738d51e9e15d1b640e3c84bff3fa7
CllaudiaB <buraanna27@gmail.com>
2025-01-31 14:03:54 +0100

    task 0

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,9 @@
 import $ from "jquery";
 
-const p1 = $("<p>").text("Holberton Dashboard");
-const p2 = $("<p>").text("Dashboard data for the students");
-const p3 = $("<p>").text("Copyright - Holberton School");
+const p1 = $("<p>Holberton Dashboard</p>");
+const p2 = $("<p>Dashboard data for the students</p>");
+const p3 = $("<p>Copyright - Holberton School</p>");
 
-$("body").append(p1, p2, p3);
+$("body").append(p1);
+$("body").append(p2);
+$("body").append(p3);
