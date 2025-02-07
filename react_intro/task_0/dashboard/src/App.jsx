a4b0604eb1bb44d3d75c0b8bf97e8146ad5af158
CllaudiaB <buraanna27@gmail.com>
2025-02-07 15:11:02 +0100

    task 0

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -2,21 +2,20 @@ import holbertonLogo from "./assets/holberton-logo.jpg";
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <div class="App-header">
-        <img src={holbertonLogo} alt="holberton logo"></img>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
-      <div class="App-body">
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div class="App-footer">
+      <div className="App-footer">
         <p>Copyright 2024 - holberton School</p>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
