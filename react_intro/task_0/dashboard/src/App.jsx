0e904c1cce7552b175bd1146403fb2a0fe7f082f
CllaudiaB <buraanna27@gmail.com>
2025-02-14 17:43:52 +0100

    task 2

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,9 +1,19 @@
 import holbertonLogo from "./assets/holberton-logo.jpg";
 import './App.css'
+import { getCurrentYear, getFooterCopy } from "../../../task_1/dashboard/src/utils";
+import Notifications from "../../../task_1/dashboard/src/Notifications";
+import "../../../task_1/dashboard/src/Notifications.css";
+
+
+const currentYear = getCurrentYear();
+const footerCopy = getFooterCopy(false);
 
 function App() {
   return (
     <>
+      <div>
+          <Notifications />
+      </div>
       <div className="App-header">
         <img src={holbertonLogo} alt="holberton logo" />
         <h1>School dashboard</h1>
@@ -12,7 +22,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2025 - holberton School</p>
+        <p>Copyright { currentYear } - { footerCopy }</p>
       </div>
     </>
   );
