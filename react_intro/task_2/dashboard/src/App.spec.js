7bb27c010de3c286afb896e32f6c032747fc84f5
CllaudiaB <buraanna27@gmail.com>
2025-02-14 20:57:45 +0100

    task 4

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -29,8 +29,8 @@ describe("App", () => {
       it("check input element", () => {
         render(<App />);
 
-        const inputEmail = screen.getByRole("textbox", {name: /email/i});
-        const inputPassword = screen.getByLabelText(/password/i);
+        const inputEmail = screen.getByLabelText(/email/i, {selector: "input"});
+        const inputPassword = screen.getByLabelText(/password/i, {selector: "input"});
 
         expect(inputEmail).toBeInTheDocument();
         expect(inputPassword).toBeInTheDocument();
