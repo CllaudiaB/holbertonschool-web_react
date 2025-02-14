57a140986d855478f85e96f73aaa0885ad6eccf9
CllaudiaB <buraanna27@gmail.com>
2025-02-14 18:06:06 +0100

    task 2

---
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -0,0 +1,28 @@
+import {render, screen} from '@testing-library/react'
+import App from './App'
+
+
+describe("App", () => {
+    it("check if the h1 element with the text School dashboard", () => {
+      render(<App />);
+  
+      const title = screen.getByRole("heading", {level:1, name:/School Dashboard/i});
+      expect(title).toBeInTheDocument();
+    });
+
+    it("check 2 p elements", () => {
+        render(<App />);
+    
+        const paragraph1 = screen.getByText(/login to access the full dashboard/i);
+        const paragraph2 = screen. getByText(/copyright 2024 - holberton school/i);
+        expect(paragraph1).toBeInTheDocument();
+        expect(paragraph2).toBeInTheDocument();
+      });
+
+      it("check if an img element is rendered", () => {
+        render(<App />);
+    
+        const image = screen.getByAltText(/holberton logo/i);
+        expect(image).toBeInTheDocument();
+      });
+  });
