9d4fd43decc057c5b3b5b0f2cee2acc3bc5e9e21
CllaudiaB <buraanna27@gmail.com>
2025-03-14 14:34:42 +0100

    task 2

--- react_props/task_2/dashboard/src/Notifications/Notifications.spec.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.spec.js
@@ -1,10 +1,17 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Notifications from './Notifications'
+
+
+const notifications = [
+    { id: 1, type: 'default', value: 'Notification 1' },
+    { id: 2, type: 'urgent', value: 'Notification 2' },
+    { id: 3, type: 'default', value: 'Notification 3' },
+];
 
 
 describe("Notifications", () => {
     it("Verify that there are 3 li elements", () => {
-        render(<Notifications />);
+        render(<Notifications notificationsList={notifications} />);
 
         const items = screen.getAllByRole("listitem")
         expect(items).toHaveLength(3)
