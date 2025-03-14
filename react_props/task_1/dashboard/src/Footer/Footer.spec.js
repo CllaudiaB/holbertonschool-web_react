7d3dd105128abf7c3f20b52f381e37790c33e504
CllaudiaB <buraanna27@gmail.com>
2025-03-14 10:46:32 +0100

    task 1

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,16 +1,12 @@
 import {render, screen} from '@testing-library/react'
 import Footer from './Footer'
-import { getCurrentYear, getFooterCopy } from "../utils/utils.js";
-
-
-const currentYear = getCurrentYear();
-const footerCopy = getFooterCopy(true);
+import { getCurrentYear } from "../utils/utils.js";
 
 describe("Footer", () => {
     it("check elements", () => {
-        render(<Footer isIndex="true"/>);
+        render(<Footer isIndex={true}/>);
     
-        const paragraph = screen.getByText(`Copyright ${currentYear} - Holberton School`);
+        const paragraph = screen.getByText(`Copyright ${getCurrentYear()} - Holberton School`);
         expect(paragraph).toBeInTheDocument();
       });
 });
