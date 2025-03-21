6b789a816573bb52851b2d50769ec15d45d6658a
CllaudiaB <buraanna27@gmail.com>
2025-03-21 19:04:37 +0100

    task 4

---
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -0,0 +1,31 @@
+import CourseListRow from "./CourseListRow";
+import './CourseList.css'
+
+
+function CourseList({ courses = [] }) {
+    return (
+        <>
+            <table id="CourseList">
+                <thead>
+                    <CourseListRow isHeader={true} textFirstCell={"Available courses"} textSecondCell={null} />
+                    <CourseListRow isHeader={true} textFirstCell={"Course name"} textSecondCell={"Credit"} />
+                </thead>
+                <tbody>
+                    {courses.length >= 1 ? (
+                        courses.map(function (cours) {
+                            return <CourseListRow
+                                key={cours.id}
+                                textFirstCell={cours.name}
+                                textSecondCell={cours.credit} />;
+                        })
+                    ) : (
+                        <CourseListRow isHeader={true} textFirstCell={"No course available yet"} textSecondCell={null} />
+                    )}
+                </tbody>
+
+            </table>
+        </>
+    );
+}
+
+export default CourseList;
