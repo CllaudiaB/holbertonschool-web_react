a9a80f2be77b3eaf944777c1c412f4e9b876c011
CllaudiaB <buraanna27@gmail.com>
2025-03-24 10:36:03 +0100

    task 5

--- react_props/task_5/dashboard/src/Notifications/Notifications.jsx
+++ react_props/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -1,56 +1,57 @@
-import { render, screen } from "@testing-library/react";
-import Notifications from "./Notifications";
+import PropTypes from "prop-types";
+import "./Notifications.css";
+import closeImage from "../assets/close-button.png";
+import NotificationItem from "./NotificationItem";
 
-describe("App", () => {
-  it("should not display close button, p element when displayDrawer is false", async () => {
-    render(<Notifications displayDrawer={false} />);
+function Notifications({ notificationsList = [], displayDrawer = false }) {
+  return (
+    <>
+      <div notifications-title>
+        <p>Your notifications</p>
+        </div>
+      {displayDrawer ? (
+        <div className="notifications">
+          {notificationsList.length === 0 ? (
+            <p>No new notification for now</p>
+          ) : (
+            <>
+              <p>Here is the list of notifications</p>
+              <button
+                aria-label="Close"
+                onClick={() => console.log("Close button has been clicked")}
+                style={{
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+              >
+                <img
+                  src={closeImage}
+                  alt="Close icon"
+                  style={{ width: "10px", height: "10px" }}
+                />
+              </button>
+              <ul>
+                {notificationsList.map((notification) => (
+                  <NotificationItem
+                    key={notification.id}
+                    type={notification.type}
+                    value={notification.value}
+                    html={notification.value}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
 
-    const button = screen.queryByAltText("Close icon");
-    const p = screen.queryByText(/here is the list of notifications/i);
-    const notificationText = screen.getByText("Your notifications");
-
-    expect(button).not.toBeInTheDocument();
-    expect(p).not.toBeInTheDocument();
-    expect(notificationText).toBeInTheDocument();
-  });
-
-  it("should display close button, p element when displayDrawer is true", async () => {
-    const notificationsList = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      {
-        id: 3,
-        type: "urgent",
-        value: "<strong>Urgent requirement</strong> - complete by EOD",
-      },
-    ];
-
-    render(
-      <Notifications
-        notificationsList={notificationsList}
-        displayDrawer={true}
-      />
-    );
-
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-
-    const p = screen.getByText(/here is the list of notifications/i);
-    expect(p).toBeInTheDocument();
-
-    const listItems = screen.getAllByRole("listitem");
-    expect(listItems).toHaveLength(3);
-
-    const notificationText = screen.getByText("Your notifications");
-    expect(notificationText).toBeInTheDocument();
-  });
-
-  it("should display displays the correct text when displayDrawer is true and notificationsList is empty", async () => {
-    render(<Notifications />);
-    const p = screen.getByText(/No new notification for now/i);
-    expect(p).toBeInTheDocument();
-
-    const notificationText = screen.getByText("Your notifications");
-    expect(notificationText).toBeInTheDocument();
-  });
-});
+export default Notifications;
