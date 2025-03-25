298e7354a20b269e1b43e58ccb8fc83706576fd1
CllaudiaB <buraanna27@gmail.com>
2025-03-25 13:34:38 +0100

    task 3

---
+++ react_component/task_3/dashboard/src/CourseList/CourseListRow.jsx
@@ -0,0 +1,14 @@
+function CourseListRow({ isHeader = false, textFirstCell = "", textSecondCell = null }) {
+    return (
+        <>
+            <tr>
+                {isHeader ? (
+                    textSecondCell === null ? <th colSpan="2">{textFirstCell}</th> :
+                    <><th>{textFirstCell}</th><th>{textSecondCell}</th></>) : 
+                    <><td>{textFirstCell}</td><td>{textSecondCell}</td></>}
+            </tr>
+        </>
+    );
+}
+
+export default CourseListRow;
