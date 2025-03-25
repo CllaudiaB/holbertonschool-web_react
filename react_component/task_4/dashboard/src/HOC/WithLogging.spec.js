c212033d6f78cbb167e712e0603f7419ecf762a8
CllaudiaB <buraanna27@gmail.com>
2025-03-25 15:54:18 +0100

    task 5

--- react_component/task_4/dashboard/src/HOC/WithLogging.spec.js
+++ react_component/task_4/dashboard/src/HOC/WithLogging.spec.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
-import withLogging from './WithLogging';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import WithLogging from "./WithLogging";
 
 class MockApp extends React.Component {
   render() {
@@ -8,29 +8,17 @@ class MockApp extends React.Component {
   }
 }
 
-describe('WithLogging HOC', () => {
-  afterEach(cleanup);
-
-  it('should render a heading element with the correct text', () => {
-    const consoleSpy = jest.spyOn(console, 'log');
-
-    const WrappedMockApp = withLogging(MockApp);
+describe("HOC", () => {
+  it("should render a heading element with the good text", () => {
+    const WrappedMockApp = WithLogging(MockApp);
 
     render(<WrappedMockApp />);
 
-    const title = screen.getByRole('heading', {
+    const title = screen.getByRole("heading", {
       level: 1,
       name: /Hello from Mock App Component/i,
     });
 
     expect(title).toBeInTheDocument();
-
-    expect(consoleSpy).toHaveBeenCalledWith('Component MockApp is mounted');
-
-    cleanup();
-
-    expect(consoleSpy).toHaveBeenCalledWith('Component MockApp is going to unmount');
-
-    consoleSpy.mockRestore();
   });
 });
