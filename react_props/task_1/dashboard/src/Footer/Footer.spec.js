943448511ebe9a47bfd4ce13fa7128bd55d6555b
CllaudiaB <buraanna27@gmail.com>
2025-04-01 14:32:04 +0200

    task 1

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -5,7 +5,13 @@ describe("Footer", () => {
   it("check elements", () => {
     render(<Footer />);
 
-    const paragraph = screen.getByText(`Copyright 2025 - Holberton School`);
+    const paragraph = screen.getByText((content, element) => {
+      return (
+        element.tagName.toLowerCase() === "p" &&
+        content.includes(`Copyright 2025 - Holberton School`)
+      );
+    });
+
     expect(paragraph).toBeInTheDocument();
   });
 });
