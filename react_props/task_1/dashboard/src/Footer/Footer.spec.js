4d801f640d02a8a607944317a821e2502324017f
CllaudiaB <buraanna27@gmail.com>
2025-04-01 13:14:08 +0200

    task 1

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,11 +1,24 @@
-import {render, screen} from '@testing-library/react'
-import Footer from './Footer'
-import { getCurrentYear, getFooterCopy} from "../utils/utils.js";
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+import { getCurrentYear, getFooterCopy } from "../utils/utils.js";
+
+jest.mock('../utils/utils.js', () => ({
+  getCurrentYear: jest.fn(),
+  getFooterCopy: jest.fn(),
+}));
 
 describe("Footer", () => {
-    it("check elements", () => {
-        render(<Footer/>); 
-        const paragraph = screen.getByText(`Copyright ${getCurrentYear()} - ${getFooterCopy(true)}`);
-        expect(paragraph).toBeInTheDocument();
-      });
+  it("check elements", () => {
+
+    const mockYear = 2025;
+    const mockFooterCopy = "Holberton School";
+
+    getCurrentYear.mockReturnValue(mockYear);
+    getFooterCopy.mockReturnValue(mockFooterCopy);
+
+    render(<Footer />);
+
+    const paragraph = screen.getByText(`Copyright ${mockYear} - ${mockFooterCopy}`);
+    expect(paragraph).toBeInTheDocument();
+  });
 });
