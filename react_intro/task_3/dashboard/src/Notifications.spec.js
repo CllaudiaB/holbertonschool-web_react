165eea5bb81da77e748d17b056a7f2679c5c9440
CllaudiaB <buraanna27@gmail.com>
2025-04-01 14:00:24 +0200

    task 3

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -1,13 +1,13 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Notifications from './Notifications'
 
 
 describe("Notifications", () => {
     it("check the existence of the notifications title Here is the list of notifications", () => {
-      render(<Notifications />);
-  
-      const title = screen.getByText(/Here is the list of notifications/i);
-      expect(title).toBeInTheDocument();
+        render(<Notifications />);
+
+        const title = screen.getByText(/Here is the list of notifications/i);
+        expect(title).toBeInTheDocument();
     });
 
     it("check button", () => {
@@ -16,11 +16,23 @@ describe("Notifications", () => {
         const button = screen.getByRole("button");
         expect(button).toBeInTheDocument();
     });
-    
+
     it("Verify that there are 3 li elements", () => {
         render(<Notifications />);
 
         const items = screen.getAllByRole("listitem")
         expect(items).toHaveLength(3)
+    });
+
+    it("logs 'Close button has been clicked' when the close button is clicked", () => {
+        render(<Notifications />);
+
+        const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+        const closeButton = screen.getByRole("button");
+        fireEvent.click(closeButton);
+
+        expect(consoleLogSpy).toHaveBeenCalledWith("Close button has been clicked");
+
+        consoleLogSpy.mockRestore();
     });
 });
