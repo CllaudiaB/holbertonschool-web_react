fc9de9588f0831263d90594c8094c2aba749d644
CllaudiaB <buraanna27@gmail.com>
2025-04-01 14:41:09 +0200

    task 1

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -3,14 +3,9 @@ import Footer from './Footer'
 
 describe("Footer", () => {
   it("check elements", () => {
-    render(<Footer isIndex={true}/>);
+    render(<Footer />);
 
-    const paragraph = screen.getByText((content, element) => {
-      return (
-        element.tagName.toLowerCase() === "p" &&
-        content.includes(`Copyright 2025 - Holberton School`)
-      );
-    });
+    const paragraph = screen.getByText(`Copyright 2025 - Holberton School`)
 
     expect(paragraph).toBeInTheDocument();
   });
