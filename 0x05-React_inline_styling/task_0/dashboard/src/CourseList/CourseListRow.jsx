b679f981eadc940d2df50645d54a26833f4dc718
CllaudiaB <buraanna27@gmail.com>
2025-04-04 15:14:20 +0200

    task 0

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.jsx
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,37 +1,41 @@
 import PropTypes from "prop-types";
 
-function CourseListRow({ isHeader = true, textFirstCell = "", textSecondCell = null }) {
+const CourseListRow = ({
+  isHeader = false,
+  textFirstCell = "",
+  textSecondCell = null,
+}) => {
+  if (isHeader === true) {
+    if (textSecondCell === null) {
+      return (
+        <tr>
+          <th colSpan="2" style={{ backgroundColor: "#deb5b545" }}>
+            {textFirstCell}
+          </th>
+        </tr>
+      );
+    } else {
+      return (
+        <tr>
+          <th style={{ backgroundColor: "#deb5b545" }}>{textFirstCell}</th>
+          <th style={{ backgroundColor: "#deb5b545" }}>{textSecondCell}</th>
+        </tr>
+      );
+    }
+  } else {
     return (
-        <>
-            <tr>
-                {isHeader ? (
-                    textSecondCell === null ? (
-                        <th colSpan={2} style={{ backgroundColor: "#deb5b545" }}>{textFirstCell}</th>
-                    ) : (
-                        <tr>
-                        <>
-                            <th style={{ backgroundColor: "#deb5b545" }}>{textFirstCell}</th>
-                            <th style={{ backgroundColor: "#deb5b545" }}>{textSecondCell}</th>
-                        </>
-                        </tr>
-                    )
-                ) : (
-                    <tr>
-                    <>
-                        <td style={{ backgroundColor: "#f5f5f5ab" }}>{textFirstCell}</td>
-                        <td style={{ backgroundColor: "#f5f5f5ab" }}>{textSecondCell}</td>
-                    </>
-                    </tr>
-                )}
-            </tr>
-        </>
+      <tr>
+        <td style={{ backgroundColor: "#f5f5f5ab" }}>{textFirstCell}</td>
+        <td style={{ backgroundColor: "#f5f5f5ab" }}>{textSecondCell}</td>
+      </tr>
     );
+  }
 };
 
 export default CourseListRow;
 
 CourseListRow.propTypes = {
-    isHeader: PropTypes.bool,
-    textFirstCell: PropTypes.string,
-    textSecondCell: PropTypes.string,
-  };
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string,
+  textSecondCell: PropTypes.string,
+};
