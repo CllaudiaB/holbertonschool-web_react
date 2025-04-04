23e41acef6bb2a21c58c75722f548d0901b54770
CllaudiaB <buraanna27@gmail.com>
2025-04-04 14:57:10 +0200

    task 0

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.jsx
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,45 +1,29 @@
-import PropTypes from "prop-types";
-
-function CourseListRow({
-  isHeader = false,
-  textFirstCell = "",
-  textSecondCell = null,
-}) {
-  return (
-    <tr>
-      {isHeader ? (
-        textSecondCell === null ? (
-          <th colSpan="2" style={{ backgroundColor: "#deb5b545" }}>
-            {textFirstCell}
-          </th>
-        ) : (
-          <>
-            <th style={{ backgroundColor: "#deb5b545" }}>
-              {textFirstCell}
-            </th>
-            <th style={{ backgroundColor: "#deb5b545" }}>
-              {textSecondCell}
-            </th>
-          </>
-        )
-      ) : (
+function CourseListRow({ isHeader = true, textFirstCell = "", textSecondCell = null }) {
+    return (
         <>
-          <td style={{ backgroundColor: "#f5f5f5ab" }}>
-            {textFirstCell}
-          </td>
-          <td style={{ backgroundColor: "#f5f5f5ab" }}>
-            {textSecondCell}
-          </td>
+            <tr>
+                {isHeader ? (
+                    textSecondCell === null ? (
+                        <th colSpan={2} style={{ backgroundColor: "#deb5b545" }}>{textFirstCell}</th>
+                    ) : (
+                        <tr>
+                        <>
+                            <th style={{ backgroundColor: "#deb5b545" }}>{textFirstCell}</th>
+                            <th style={{ backgroundColor: "#deb5b545" }}>{textSecondCell}</th>
+                        </>
+                        </tr>
+                    )
+                ) : (
+                    <tr>
+                    <>
+                        <td style={{ backgroundColor: "#f5f5f5ab" }}>{textFirstCell}</td>
+                        <td style={{ backgroundColor: "#f5f5f5ab" }}>{textSecondCell}</td>
+                    </>
+                    </tr>
+                )}
+            </tr>
         </>
-      )}
-    </tr>
-  );
-}
-
-CourseListRow.propTypes = {
-  isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string,
-  textSecondCell: PropTypes.string,
+    );
 };
 
 export default CourseListRow;
