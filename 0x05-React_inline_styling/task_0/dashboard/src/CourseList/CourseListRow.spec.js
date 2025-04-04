0926bac83d2dae991f665a22c250bc9346ce4a07
CllaudiaB <buraanna27@gmail.com>
2025-04-04 15:02:59 +0200

    task 0

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.spec.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.spec.js
@@ -46,6 +46,52 @@ describe("CourseListRow Header is False", () => {
     expect(tdElements).toHaveLength(2);
   });
 
+  it("the cell background color is #deb5b545", async () => {
+    render(
+      <CourseListRow
+        isHeader={true}
+        textSecondCell={null}
+        textFirstCell="Test course list row"
+      />
+    );
+
+    const thElement = screen.getByRole("columnheader", {
+      name: "Test course list row",
+    });
+
+    expect(thElement).toHaveStyle(
+      `background-color: rgba(222, 181, 181, 0.271)`
+    );
+  });
+
+  it("isHeader prop is true and secondTextCell is not null, the cell background color is #deb5b545", async () => {
+    render(
+      <CourseListRow
+        isHeader={true}
+        textSecondCell="Test second cell of course list row"
+        textFirstCell="Test first cell of course list row"
+      />
+    );
+
+    const thElements = screen.getAllByRole("columnheader");
+
+    expect(thElements[0]).toHaveStyle(
+      `background-color: rgba(222, 181, 181, 0.271)`
+    );
+  });
+});
+
+describe("CourseListRow Header is False", () => {
+  it("should render two td elements within a tr element", async () => {
+    render(<CourseListRow isHeader={false} />);
+
+    const trElement = screen.getByRole("row");
+    const tdElements = within(trElement).getAllByRole("cell");
+
+    expect(trElement).toBeInTheDocument();
+    expect(tdElements).toHaveLength(2);
+  });
+
   it("the cell background color is #f5f5f5ab ", async () => {
     render(<CourseListRow isHeader={false} />);
     const trElement = screen.getByRole("row");
@@ -55,5 +101,4 @@ describe("CourseListRow Header is False", () => {
       `background-color: rgba(245, 245, 245, 0.671)`
     );
   });
-
 });
