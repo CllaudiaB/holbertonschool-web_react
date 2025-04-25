d9ea932372ef11f95b190f665917f7e03111beb9
CllaudiaB <buraanna27@gmail.com>
2025-04-25 16:43:22 +0200

    task 2

--- react_props/task_2/dashboard/src/Notifications/Notifications.spec.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.spec.js
@@ -21,7 +21,7 @@ describe("Notifications", () => {
     const testNotifications = [
         { id: 1, type: "default", value: "New course available" },
         { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", value: getLatestNotification() },
+        { id: 3, type: "urgent", html: getLatestNotification()},
         ];
     const { getByText } = render(<Notifications notificationsList={testNotifications}/>);
 
